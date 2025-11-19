0c6ff11f6e1bc9387aba600a392997a6a678ab03
Jason Murphy <jasonhmurphy@gmail.com>
2025-11-19 11:23:28 -0500

    add CORS to flask app

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/scores', methods=['GET'])
 def get_scores():
